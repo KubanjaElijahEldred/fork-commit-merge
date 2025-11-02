895bf406fc7ffe428142058fdaca59c8a9dc7520
kubanjaelijaheldred <kubanjaelijah2037@gmail.com>
2025-11-02 17:25:33 +0300

    Fix-Julia-sum-task

--- tasks/julia/easy/sum_function.jl
+++ tasks/julia/easy/sum_function.jl
@@ -1,3 +1,10 @@
 # Julia - Easy
 
-# TODO: Implement the function required in the problem description here
+# Function that returns the sum of two numbers
+function sum_two_numbers(a, b)
+    return a + b
+end
+
+# Test the function
+result = sum_two_numbers(5, 3)
+println("Sum: ", result)
