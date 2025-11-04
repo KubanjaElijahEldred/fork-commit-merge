f92e6d433fecf6d68b7ccc96e3aa14758e5c5434
kubanjaelijaheldred <kubanjaelijah2037@gmail.com>
2025-11-04 13:45:16 +0300

    Fix-Lisp-factorial-task

--- tasks/lisp/easy/factorial.lisp
+++ tasks/lisp/easy/factorial.lisp
@@ -1,5 +1,7 @@
 ;; Lisp - Easy
 
 (defun factorial (n)
-  ;; TODO: Implement the factorial function
-)
+  ;; Implement the factorial function
+  (if (<= n 1)
+      1
+      (* n (factorial (- n 1)))))
