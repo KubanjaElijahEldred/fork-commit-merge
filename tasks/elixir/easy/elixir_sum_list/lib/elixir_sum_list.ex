b668044026de2303f446c334f61dd7bf706be267
kubanjaelijaheldred <kubanjaelijah2037@gmail.com>
2025-11-05 19:49:40 +0300

    Fix-Elixir-sum-list-task

--- tasks/elixir/easy/elixir_sum_list/lib/elixir_sum_list.ex
+++ tasks/elixir/easy/elixir_sum_list/lib/elixir_sum_list.ex
@@ -1,5 +1,6 @@
 # Elixir - Easy
 
 defmodule ElixirSumList do
-  # TODO: Implement a Basic Elixir Function to Sum a List of Numbers
+  def sum_list([]), do: 0
+  def sum_list([head | tail]), do: head + sum_list(tail)
 end
